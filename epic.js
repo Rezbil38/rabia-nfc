d9ba1e7d7a07911b20599e6550c6049d57723ef6
Rezbil38 <101962467+Rezbil38@users.noreply.github.com>
2026-09-10 15:43:33 -0500

    Add files via upload

---
+++ epic.js
@@ -0,0 +1,6 @@
+const images = {
+    step1: "URL_O_NOMBRE_DE_TU_IMAGEN_1.jpg",
+    step2: "URL_O_NOMBRE_DE_TU_IMAGEN_2.jpg",
+    optionYes: "URL_O_NOMBRE_DE_TU_IMAGEN_SI.jpg",
+    optionNo: "URL_O_NOMBRE_DE_TU_IMAGEN_NO.jpg"
+};
